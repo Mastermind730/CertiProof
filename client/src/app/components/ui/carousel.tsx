5a4e5fcc47c3963288677255d4e02b5398d650ef
Mastermind730 <sourav.97032@gmail.com>
2025-12-03 20:02:59 +0530

    package fix

--- client/src/app/components/ui/carousel.tsx
+++ client/src/app/components/ui/carousel.tsx
@@ -2,14 +2,15 @@
 
 import * as React from 'react'
 import useEmblaCarousel, {
-  type UseEmblaCarouselType,
-} from 'embla-carousel-react'
+  type EmblaCarouselType,
+} from 'embla-carousel'
 import { ArrowLeft, ArrowRight } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
+import { Button } from './button'
 
-type CarouselApi = UseEmblaCarouselType[1]
+
+type CarouselApi = EmblaCarouselType[1]
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>
 type CarouselOptions = UseCarouselParameters[0]
 type CarouselPlugin = UseCarouselParameters[1]
